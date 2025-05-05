ce00d1fe24375fe8b4f6c0d9cbc3bf9338f738f4
Alois D'Alto <aloisdalto@gmail.com>
2025-05-05 14:28:11 -0400

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <nav className="bg-dark text-white text-center p-3">
-            <h3>Prueba Tecnica Frontend</h3>
+            <h3>Prueba Tecnica Frontend - Alois D'Alto</h3>
         </nav>
     );
 };
